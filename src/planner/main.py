3f811ac3b956eeeaa768cb9ece49892cd8ad654a
safecorners <safecorners.mobile@gmail.com>
2024-02-17 21:55:18 +0900

    planner: implement persistence layer using sqlmodel

--- src/planner/main.py
+++ src/planner/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
+from planner.database.connection import conn
 from planner.routes.events import event_router
 from planner.routes.users import user_router
 
@@ -7,3 +9,13 @@ app = FastAPI()
 
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
+
+
+@app.on_event("startup")
+def on_startup() -> None:
+    conn()
+
+
+@app.get("/")
+async def home() -> RedirectResponse:
+    return RedirectResponse(url="/event/")
--- src/planner/routes/events.py
+++ src/planner/routes/events.py
@@ -1,8 +1,10 @@
-from typing import Dict, List
+from typing import Any, Callable, Dict, List, Union, cast
 
-from fastapi import APIRouter, Body, HTTPException, status
+from fastapi import APIRouter, Body, Depends, HTTPException, Request, status
+from sqlmodel import Session, select
 
-from planner.models.events import Event
+from planner.database.connection import get_session
+from planner.models.events import Event, EventUpdate
 
 event_router = APIRouter(tags=["Events"])
 
@@ -10,15 +12,18 @@ events: List[Event] = []
 
 
 @event_router.get("/", response_model=List[Event])
-async def retrieve_all_events() -> List[Event]:
-    return events
+async def retrieve_all_events(session: Session = Depends(get_session)) -> List[Event]:
+    statement = select(Event)
+    events = session.exec(statement).all()
+    return list(events)
 
 
 @event_router.get("/{id}", response_model=Event)
-async def retrieve_event(id: int) -> Event:
-    for event in events:
-        if event.id == id:
-            return event
+async def retrieve_event(id: int, session: Session = Depends(get_session)) -> Event:
+    event = session.get(Event, id)
+    if event:
+        return event
+
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Event with supplied ID does not exist",
@@ -26,17 +31,46 @@ async def retrieve_event(id: int) -> Event:
 
 
 @event_router.post("/new")
-async def create_event(event: Event = Body(...)) -> Dict[str, str]:
-    events.append(event)
+async def create_event(
+    new_event: Event = Body(...), session: Session = Depends(get_session)
+) -> Dict[str, str]:
+    session.add(new_event)
+    session.commit()
+    session.refresh(new_event)
+
     return {"message": "Event created successfully"}
 
 
+@event_router.put("/edit/{id}", response_model=Event)
+async def update_event(
+    id: int, new_data: EventUpdate, session: Session = Depends(get_session)
+) -> Event:
+    event = session.get(Event, id)
+    if event:
+        event_data = new_data.dict(exclude_unset=True)
+        for key, value in event_data.items():
+            setattr(event, key, value)
+        session.add(event)
+        session.commit()
+        session.refresh(event)
+        return event
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Event with supplied ID does not exist",
+    )
+
+
 @event_router.delete("/{id}")
-async def delete_event(id: int) -> Dict[str, str]:
-    for event in events:
-        if event.id == id:
-            events.remove(event)
-            return {"message": "Event deleted successfully"}
+async def delete_event(
+    id: int, session: Session = Depends(get_session)
+) -> Dict[str, str]:
+    event = session.get(Event, id)
+    if event:
+        session.delete(event)
+        session.commit()
+        return {"message": "Event deleted successfully"}
+
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Event with supplied ID doeest not exist",
