5f2145cfa28a0bdd0792def742d3af53ad4bb931
safecorners <safecorners.mobile@gmail.com>
2024-05-27 09:11:38 +0900

    change database connection method to support async

--- src/planner/database/database.py
+++ src/planner/database/database.py
@@ -1,9 +1,14 @@
 import logging
-from contextlib import contextmanager
-from typing import Iterator
+from asyncio import current_task
+from contextlib import asynccontextmanager
+from typing import AsyncIterator
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, scoped_session, sessionmaker
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_scoped_session,
+    async_sessionmaker,
+    create_async_engine,
+)
 
 from planner.database.orm import Base
 
@@ -12,22 +17,28 @@ logger = logging.getLogger(__name__)
 
 class Database:
     def __init__(self, db_url: str) -> None:
-        self._engine = create_engine(db_url, echo=True)
-        self._session_factory = scoped_session(
-            sessionmaker(autocommit=False, autoflush=False, bind=self._engine)
+        self._engine = create_async_engine(db_url, echo=True)
+        self._session_factory = async_scoped_session(
+            async_sessionmaker(autocommit=False, autoflush=False, bind=self._engine),
+            scopefunc=current_task,
         )
 
-    def create_database(self) -> None:
-        Base.metadata.create_all(self._engine)
+    async def create_database(self) -> None:
+        async with self._engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
 
-    @contextmanager
-    def session(self) -> Iterator[Session]:
-        session: Session = self._session_factory()
+    async def drop_database(self) -> None:
+        async with self._engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+
+    @asynccontextmanager
+    async def session(self) -> AsyncIterator[AsyncSession]:
+        session: AsyncSession = self._session_factory()
         try:
             yield session
         except Exception:
             logger.exception("Session rollback because of exception")
-            session.rollback()
+            await session.rollback()
             raise
         finally:
-            session.close()
+            await session.close()
